cce493028ad7425063c3b6727a45d5afda782023
Saugat <saugatghimire321@gmail.com>
2022-11-12 10:41:25 +0545

    User

---
+++ routes/userRoutes.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const userController =require('../controllers/userController')
+
+
+const router = express.Router()
+const userController1= new userController();
+
+router.post('/signup',userController1.addUser);
+router.get('/',userController1.getAllUsers);
+router.get('/:id',userController1.getSingleUser);
+router.put('/:id',userController1.updateUser);
+router.delete('/:id',userController1.deleteUser);
+
+// router.post('/login',user.login);
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const dotenv = require("dotenv/config");
+const cors = require('cors');
+const mongoose = require('mongoose')
+const userRoutes = require('./routes/userRoutes')
+
+const app = express();
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+app.use(express.static("public"));
+app.get("/", (req, res) => {
+  res.send("backend is working 👋");
+});
+
+app.use('/user',userRoutes);
+
+
+app.listen(process.env.PORT || 8000, async () => {
+  console.log("Server has started");
+  try {
+    mongoose.connect(process.env.conn_str);
+    console.log("Successfully Connected to db 🚀🚀");
+  } catch (err) {
+    console.log("❌❌Error during connection to database❌❌:", err);
+  }
+});

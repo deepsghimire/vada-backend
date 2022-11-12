5b8797bb8f76a39b1382e518bc3c21cca5b5985b
Saugat <saugatghimire321@gmail.com>
2022-11-12 16:00:23 +0545

    login

--- controllers/userController.js
+++ controllers/userController.js
@@ -27,6 +27,22 @@ module.exports =  class UserController{
             res.json(err);
         }
     }
+    //login user
+    async loginUser(req,res){
+        const body = req.body;
+    const user = await User.findOne({ email: body.email });
+    if (user) {
+      // check user password with hashed password stored in the database
+      const validPassword = await bcrypt.compare(body.password, user.password);
+      if (validPassword) {
+        res.status(200).json({ message: "Valid password" });
+      } else {
+        res.status(400).json({ error: "Invalid Password" });
+      }
+    } else {
+      res.status(401).json({ error: "User does not exist" });
+    }
+    }
 
     //get all user details
     async getAllUsers(req,res,next){
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,6 +6,7 @@ const router = express.Router()
 const userController1= new userController();
 
 router.post('/signup',userController1.addUser);
+router.post("/login",userController1.loginUser);
 router.get('/',userController1.getAllUsers);
 router.get('/:id',userController1.getSingleUser);
 router.put('/:id',userController1.updateUser);
